7eda603b8bfc490520d5467b2f7822c9e1057321
Boss <boss@excel-cookbook.com>
2026-04-15 20:00:51 +1000

    fix: remove unsupported eslint config from NextConfig in Next.js 16

--- next.config.ts
+++ next.config.ts
@@ -13,11 +13,6 @@ const nextConfig: NextConfig = {
     // !! WARN !!
     ignoreBuildErrors: true,
   },
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
   images: {
     unoptimized: true,
   },
